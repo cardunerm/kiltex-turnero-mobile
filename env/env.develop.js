70c60c150a10cba0f28dcc46c0701af505ccb5c0
rociolesc <rocio.lescano@sandinas.com.ar>
2023-03-03 11:42:13 -0300

    Modificacion apk2 3/3/2023

--- env/env.develop.js
+++ env/env.develop.js
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     api: {
-      url: 'https://caught-customs-worthy-electron.trycloudflare.com'
+      url: 'https://www.monitor.kiltex.com.ar'
       //url: 'https://localhost:5001'
       //url: 'https://www.monitor.kiltex.com.ar'
     }
